e3cf9b9aceaba849bbbcd4dfe1e5bd6b319ede8b
Vincent <vzorge@linuse.nl>
2023-07-11 20:02:41 +0200

    Move incorrect vite config in svelte.config.js

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+    optimizeDeps: {
+        include: ['papaparse']
+    },
     server: {
         proxy: {
             '/api': 'http://localhost:8080',
