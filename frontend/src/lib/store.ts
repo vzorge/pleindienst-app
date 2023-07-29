1bc161452cc758e9b588fdde99694589faef5e1b
Vincent <vzorge@linuse.nl>
2023-07-29 22:24:02 +0200

    Own serializer to handle undefined properly

--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -7,4 +7,9 @@ export const persons = localStorageStore<Person[]>('persons', []);
 
 export const group = localStorageStore<Group>('group', {} as Group);
 
-export const resultStore = localStorageStore<MatchingResponse | undefined>('result', undefined);
+export const resultStore = localStorageStore<MatchingResponse | undefined>('result', undefined, {
+    serializer: {
+        parse: (text: string) => (text && text === 'undefined') ? undefined : JSON.parse(text) as MatchingResponse,
+        stringify: (object: MatchingResponse | undefined): string => object ? JSON.stringify(object) : 'undefined'
+    }
+});
