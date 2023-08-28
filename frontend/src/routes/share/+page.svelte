db9e2b6146e7f61973261095aa363ae8a514d010
Vincent <vzorge@linuse.nl>
2023-08-28 19:37:36 +0200

    share page. Nog zonder data

--- frontend/src/routes/share/+page.svelte
+++ frontend/src/routes/share/+page.svelte
@@ -6,6 +6,8 @@
 	import { createCalendarEvent, downloadCalendarEvent } from "$lib/Calendar";
 	import { overblijftijd } from "$lib/data/overblijftime";
 
+  const filterValues = ['vakantie', 'tudiedag', '12 uur', 'Paas', '2e', 'vrije dag']
+
   const DATE_FIELD = 'Datum';
   const GROUP_FIELD = 'Groep';
   const NAAM_FIELD = 'Ouders van:';
@@ -13,10 +15,12 @@
   type DataRow = {date: Date, group: Group, name: string};
 
   let names: {[key:string]: DataRow[]} = {};
+  let searchInput = '';
+  $: filteredNames = Object.entries(names).filter(([name, _]) => searchInput === '' ? true : name.includes(searchInput))
 
   async function getResults() {
     const data: any = await new Promise((resolve) => {
-      papaparse.parse('/csv/pleindienst-BB1.csv', {
+      papaparse.parse('/pleindienst-BB1.csv', {
         download: true,
         delimiter: ';',
         header: true,
@@ -37,7 +41,7 @@
         group: parseGroup(d[GROUP_FIELD]),
         name: d[NAAM_FIELD]
       }))
-      .filter((d: DataRow) => !(d.name.includes('vakantie') || d.name.includes('Studiedag') || d.name.includes('12 uur')))
+      .filter((d: DataRow) => !(filterValues.some(val => d.name.includes(val))))
       .reduce(groupBy, {});
     console.log(names);
   }
@@ -59,16 +63,24 @@
   });
 </script>
 
-
-<div class="flex flex-col flex-wrap space-y-5">
-  {#each Object.entries(names) as [name, data]}
-  <div class="flex space-x-7">
-    <button class="btn btn-icon btn-icon-sm variant-filled-primary" on:click={() => createCalendarEvents(name, data)}>
-      <img src={downloadImg} alt="Download" class="h-2/3"/>
-    </button>
-    <span>{name}</span>
+<div class="flex flex-col items-center mt-6 space-y-6">
+  <span>Download een .ics file om makkelijk alle overblijfdagen van de Montessori Leidschenveen in uw agenda te zetten.</span>
+  <div class="flex flex-col flex-wrap space-y-5">
+    <div class="flex">
+      <label class="label">
+        <span>Filter</span>
+        <input type="text" bind:value={searchInput} class="input"/>
+      </label>
+    </div>
+    {#each filteredNames as [name, data]}
+    <div class="flex space-x-7">
+      <button class="btn btn-icon btn-icon-sm variant-filled-primary" on:click={() => createCalendarEvents(name, data)}>
+        <img src={downloadImg} alt="Download" class="h-3/5"/>
+      </button>
+      <span>{name}</span>
+    </div>
+    <hr class="!border-dashed"/>
+    
+    {/each}
   </div>
-  <hr class="!border-dashed"/>
-  
-  {/each}
 </div>
